4f6323771e97e2b1f198251b076e02b5c8497d1f
Nono <marvin.jeannot@iowizmi.fr>
2023-04-04 16:07:16 +0200

    [DOC] add en

---
+++ en/doc.h
@@ -0,0 +1,101 @@
+/**
+  * @file: doc.h
+  * @brief
+  *
+  * @details
+  *
+  * @copyright 2019, GRDF, Inc.  All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without
+  * modification, are permitted (subject to the limitations in the disclaimer
+  * below) provided that the following conditions are met:
+  *    - Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *    - Redistributions in binary form must reproduce the above copyright
+  *      notice, this list of conditions and the following disclaimer in the
+  *      documentation and/or other materials provided with the distribution.
+  *    - Neither the name of GRDF, Inc. nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  *
+  * @par Revision history
+  *
+  * @par 1.0.0 : 2022/02/16 [GBI]
+  * Initial version
+  *
+  */
+
+/*!
+ * @mainpage OpenWize
+ *
+ * <img src="logo_Wize_alliance.png" align="left">
+ * <div style="clear: both"></div>
+ *
+ * @defgroup wize_core WizeCore
+ * @{
+ *		@defgroup wize_api Wize API
+ * 		@defgroup wize_app Application Layer
+ * 		@{
+ * 			@defgroup wize_comm_app Common Application Layer
+ * 			@{
+ *	 			@defgroup wize_inst_layer Installation Layer
+ *				@defgroup wize_admin_layer Administration Layer
+ *				@defgroup wize_dwn_layer Download Layer
+ * 			@}
+ * 		@}
+ *		@defgroup wize_time_mgr Time management
+ * 		@defgroup wize_mgr Sessions Management
+ * 		@{
+ *			@defgroup wize_admin_mgr Administration Session
+ *	 		@defgroup wize_inst_mgr Installation Session
+ *			@defgroup wize_dwn_mgr Download Session
+ * 		@}
+ * 		@defgroup wize_net Network Device Management
+ * 		@{
+ *			@defgroup wize_net_mgr Network Device Manager
+ *	 		@defgroup wize_net_api Network API
+ *	 		@defgroup wize_phy_itf Phy Interface
+ * 		@}
+ * 		@defgroup wize_proto Protocol
+ * @}
+ *
+ * @defgroup Samples Samples
+ * @{
+ * 		@defgroup crc_sw CRC sw
+ * 		@defgroup crypto Crypto
+ * 		@defgroup reed_solomon Reed-Solomon
+ * 		@defgroup parameters Parameters
+ * 		@defgroup time_evt Time Event
+ * 		@defgroup ing_storage Image Storage
+ * 		@defgroup logger Logger
+ * @}
+ *
+ * @cond DOC_DEMO
+ * @{
+ * 		@defgroup demo Demo
+ * 		@{
+ * 			@defgroup nucleo_L476 Nucleo L476RG
+ * 			@{
+ * 				@defgroup nucleo_L476_app Application
+ * 				@defgroup nucleo_L476_bsp Board Support Package
+ * 				@defgroup nucleo_L476_board Board
+ * 				@defgroup nucleo_L476_device Devices
+ * 			@}
+ * 			@cond DOC_DEMO_SIMU_X86
+ * 			@{
+ *	 			@defgroup simu_x86 Simulator x86
+ * 				@{
+ * 					@defgroup simu_x86_app Application
+ * 					@defgroup simu_x86_bsp Board Support Package
+ * 					@defgroup simu_x86_board Board
+ * 					@defgroup simu_x86_device Devices
+ * 				@}
+ * 			@endcond
+ * 			@}
+ * 		@}
+ * @endcond
+ * @}
+ *
+ *
+ */
